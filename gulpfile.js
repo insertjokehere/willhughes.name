c28bc38141afc2860e6a93d3e2cd69d4d0f6713a
William Hughes <whlabratz@gmail.com>
2016-03-12 16:38:28 +1300

    Minify PNGs

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var compress = require('gulp-yuicompressor');
 var shell = require('gulp-shell');
 var htmlmin = require('gulp-htmlmin');
 var indexer = require('./indexer.js');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
 
 gulp.task('default', ['release', 'dev'], function () {});
 
-gulp.task('release', ['hugo', 'mini_js', 'mini_css', 'mini_html'], function () {});
+gulp.task('release', ['hugo', 'mini_js', 'mini_css', 'mini_html', 'mini_png'], function () {});
 
 gulp.task('dev', ['index'], function () {
     gulp.src('public/search.json').pipe(gulp.dest('static/'));
@@ -34,6 +36,16 @@ gulp.task('mini_html', ['hugo'], function() {
   return gulp.src('public/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('public'))
+});
+
+gulp.task('mini_png', ['hugo'], function() {
+    return gulp.src('public/images/*.png')
+	.pipe(imagemin({
+	    progressive: true,
+	    svgoPlugins: [{removeViewBox: false}],
+	    use: [pngquant()]
+	}))
+	.pipe(gulp.dest('public/images/'))
 });
 
 gulp.task('index', ['hugo'], function () {
