0b8068ddd3165557a819ea5f32491e266b252210
William Hughes <whlabratz@gmail.com>
2016-06-28 15:27:02 +1200

    Update dev script so dynamic stuff gets build as well

--- dev.sh
+++ dev.sh
@@ -2,4 +2,6 @@
 
 docker build -t willhughes.name .
 
+docker run -v `pwd`:/mnt/data -v $HOME:/home --user `id -u`:`id -g` --entrypoint="gulp" willhughes.name
+
 docker run -v `pwd`:/mnt/data -p 1313:1313 willhughes.name serve --bind 0.0.0.0 --buildDrafts
