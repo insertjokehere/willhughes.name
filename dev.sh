c5f8c673343af501b9ea7052f448b80579fa5413
William Hughes <whlabratz@gmail.com>
2016-09-22 12:12:33 +1200

    Tweak dev.sh to make live content updates work

--- dev.sh
+++ dev.sh
@@ -2,4 +2,4 @@
 
 docker build -t willhughes.name .
 
-docker run --rm -ti -v `pwd`:/mnt/data --user 106:106 -p 1313:1313 willhughes.name /bin/bash -c "cp -r /mnt/data/* /home/jenkins && cd /home/jenkins && gulp && hugo serve --bind 0.0.0.0 --buildDrafts"
+docker run --rm -ti -v `pwd`:/mnt/data --user `id -u`:`id -g` -p 1313:1313 willhughes.name /bin/bash -c "cd /mnt/data && gulp && hugo serve --bind 0.0.0.0 --buildDrafts"
