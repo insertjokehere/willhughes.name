afd7c3bab285c623d1b1e140bc7353f1e03e505e
William Hughes <williamh@catalyst.net.nz>
2019-04-11 16:21:08 +1200

    Update for Hugo 0.55

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/bash -e
 
-docker build -t willhughes.name .
+docker build --pull -t willhughes.name .
 
 docker run --rm -ti -v `pwd`:/mnt/data --user `id -u`:`id -g` -p 0.0.0.0:1313:1313 willhughes.name /usr/local/bin/whn_dev.sh
